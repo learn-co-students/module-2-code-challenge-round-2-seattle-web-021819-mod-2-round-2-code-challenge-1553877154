f62a3a536973c78c50f91fb50151406ea30af204
github username <github email address>
2019-03-29 11:00:39 -0700

    finished test

---
+++ app/models/appearance.rb
@@ -0,0 +1,6 @@
+class Appearance < ApplicationRecord
+  belongs_to :episode
+  belongs_to :guest
+
+  validates :guest_rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,6 @@
 class Episode < ApplicationRecord
+  has_many :appearances
+  has_many :guests, through: :appearances
+
+  # validates :guest_rating, numericality{1..5}
 end
